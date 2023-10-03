490068e3b911fc0f1cbe5d53f52eadd8c79b413c
Jay Choy <zcabzjc@ucl.ac.uk>
2023-10-03 11:54:57 +0100

    inital linter

--- Main/main.go
+++ Main/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"go/ast"
 	"go/parser"
-	"go/printer"
 	"go/token"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -32,13 +31,44 @@ type visitor struct {
 }
 
 func (v *visitor) Visit(node ast.Node) ast.Visitor {
-	if node == nil {
-		return nil
+	funcDecl, ok := node.(*ast.FuncDecl)
+	if !ok {
+		return v
 	}
 
-	var buf bytes.Buffer
-	printer.Fprint(&buf, v.fset, node)
-	fmt.Printf("%s | %#v\n", buf.String(), node)
+	params := funcDecl.Type.Params.List
+	if len(params) != 2 { // [0] must be format (string), [1] must be args (...interface{})
+		return v
+	}
 
- 	return v
+	firstParamType, ok := params[0].Type.(*ast.Ident)
+	if !ok { // first param type isn't identificator so it can't be of type "string"
+		return v
+	}
+
+	if firstParamType.Name != "string" { // first param (format) type is not string
+		return v
+	}
+
+	secondParamType, ok := params[1].Type.(*ast.Ellipsis)
+	if !ok { // args are not ellipsis (...args)
+		return v
+	}
+
+	elementType, ok := secondParamType.Elt.(*ast.InterfaceType)
+	if !ok { // args are not of interface type, but we need interface{}
+		return v
+	}
+
+	if elementType.Methods != nil && len(elementType.Methods.List) != 0 {
+		return v // has >= 1 method in interface, but we need an empty interface "interface{}"
+	}
+
+	if strings.HasSuffix(funcDecl.Name.Name, "f") {
+		return v
+	}
+
+	fmt.Printf("%s: printf-like formatting function '%s' should be named '%sf'\n",
+		v.fset.Position(node.Pos()), funcDecl.Name.Name, funcDecl.Name.Name)
+	return v
 }
